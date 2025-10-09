c0e5d8b78c2f12b397b2536500b94c223acbc361
ZenithMeetsNadir <martin-vlcek@post.cz>
2025-10-09 17:59:27 +0200

    fix the stupidest implementation of radix sort

--- lib/sort.c
+++ lib/sort.c
@@ -1,33 +1,38 @@
 #include "sort.h"
 
 char radixSort(uint64_t *data, size_t size) {
-    uint64_t count_buf[ITER_DEPTH];
-    for (size_t i = 0; i < ITER_DEPTH; i++)
-        count_buf[i] = 0;
-
     uint64_t *buck_buf = (uint64_t *)malloc(size * sizeof(uint64_t));
     if (buck_buf == NULL)
         return 0;
 
+    uint64_t max = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (data[i] > max)
+            max = data[i];
+    }
+
+    uint64_t count_buf[ITER_SIZE];
     char in_buf_out = 1;
     int shift = 0;
-    size_t num_zeroes = 0;
-    while (shift < 64 && count_buf[0] != size) {
+    while (shift < INT_BITS && max >> shift > 0) {
         in_buf_out = !in_buf_out;
 
         uint64_t *in_buf = in_buf_out ? buck_buf : data;
         uint64_t *out_buf = in_buf_out ? data : buck_buf;
 
-        for (size_t i = 0; i < size; i++)
-            count_buf[in_buf[i] & (ITER_DEPTH << shift)]++;
+        for (size_t i = 0; i < ITER_SIZE; i++)
+            count_buf[i] = 0;
 
-        for (size_t i = 1; i < ITER_DEPTH; i++)
+        for (size_t i = 0; i < size; i++)
+            count_buf[(in_buf[i] & (ITER_DEPTH << shift)) >> shift]++;
+
+        for (size_t i = 1; i < ITER_SIZE; i++)
             count_buf[i] += count_buf[i - 1];
 
         for (size_t i = 0; i < size; i++)
-            out_buf[-1 + count_buf[in_buf[i] & (ITER_DEPTH << shift)]--] = in_buf[i];
+            out_buf[-1 + count_buf[(in_buf[size - 1 - i] & (ITER_DEPTH << shift)) >> shift]--] = in_buf[size - 1 - i];
 
-        shift += ITER_DEPTH / 8;
+        shift += ITER_BITS;
     }
 
     if (!in_buf_out) {
@@ -39,15 +44,14 @@ char radixSort(uint64_t *data, size_t size) {
     return 1;
 }
 
-uint64_t* excludeZeroes(const uint64_t *data, size_t size, size_t *out_size) {
+uint64_t* excludeZeroesLeft(const uint64_t *data, size_t size, size_t *out_size) {
     size_t left = 0, right = size;
     while (left < right) {
-        size_t mid = left + (right - left) / 2;
-        if (data[mid] == 0) {
+        size_t mid = (left + right) / 2;
+        if (data[mid] == 0)
             left = mid + 1;
-        } else {
+        else
             right = mid;
-        }
     }
 
     if (left < size && data[left] != 0) {
@@ -57,4 +61,20 @@ uint64_t* excludeZeroes(const uint64_t *data, size_t size, size_t *out_size) {
 
     *out_size = 0;
     return NULL;
+}
+
+size_t indexOfAscending(const uint64_t *data, size_t size, uint64_t value) {
+    size_t left = 0, right = size;
+    while (left < right) {
+        size_t mid = (left + right) / 2;
+        if (data[mid] < value)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+
+    if (left < size && data[left] == value)
+        return left;
+
+    return size;
 }
--- lib/sort.h
+++ lib/sort.h
@@ -4,10 +4,21 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <math.h>
+#include <stdio.h>
 
-#define ITER_DEPTH 0xff
+#define ITER_BITS 8
+#define ITER_SIZE (1 << ITER_BITS)
+#define ITER_DEPTH (ITER_SIZE - 1)
+#undef LITTLE_ENDIAN
+#define LITTLE_ENDIAN (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define INT_BITS (sizeof(uint64_t) * 8)
 
 char radixSort(uint64_t *data, size_t size);
-uint64_t* excludeZeroes(const uint64_t *data, size_t size, size_t *out_size);
+
+/// offset the start of an ascending sorted array to the first non-zero element
+uint64_t* excludeZeroesLeft(const uint64_t *data, size_t size, size_t *out_size);
+
+/// return size if not found
+size_t indexOfAscending(const uint64_t *data, size_t size, uint64_t value);
 
 #endif
